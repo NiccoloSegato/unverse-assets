f9a5c097df4dd959b8b871e99d620ffed105301c
NiccoloSegato <segato117@gmail.com>
2025-07-07 01:24:19 +0200

    Reset colore embossing

--- scripts/export.js
+++ scripts/export.js
@@ -26,7 +26,7 @@ export const DDC_embossingStartColors = [
 export const DDC_embossingColors = [
     '#172966',
     '#aec7d6',
-    'transparent',
+    '#000000',
     '#322e45'
 ];
 export const DDC_embossingShadows = [
